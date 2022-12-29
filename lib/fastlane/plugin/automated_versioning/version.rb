53ccb1439d4ef3fec59b93b9bbb24b2400f14917
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-29 14:20:53 +0100

    initial commit

---
+++ lib/fastlane/plugin/automated_versioning/version.rb
@@ -0,0 +1,5 @@
+module Fastlane
+  module AutomatedVersioning
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/automated_versioning_action_spec.rb
@@ -0,0 +1,9 @@
+describe Fastlane::Actions::AutomatedVersioningAction do
+  describe '#run' do
+    it 'prints a message' do
+      expect(Fastlane::UI).to receive(:message).with("The automated_versioning plugin is working!")
+
+      Fastlane::Actions::AutomatedVersioningAction.run(nil)
+    end
+  end
+end
