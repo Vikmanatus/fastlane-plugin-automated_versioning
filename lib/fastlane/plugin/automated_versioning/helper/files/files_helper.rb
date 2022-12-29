96896afe775eec7fbdd34e7123cb796c91f2e930
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-29 16:04:04 +0100

    WIP: adding required check for config file

---
+++ lib/fastlane/plugin/automated_versioning/helper/files/files_helper.rb
@@ -0,0 +1,19 @@
+require "fastlane_core/ui/ui"
+
+module Fastlane
+  UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
+
+  module Helper
+    class FilesHelper
+      def self.check_file_existence(file_path:)
+        relative_file_path = File.join(File.expand_path(file_path))
+        is_file_created = File.file?(relative_file_path)
+        if is_file_created
+          return TRUE
+        else
+          return FALSE
+        end
+      end
+    end
+  end
+end
--- lib/fastlane/plugin/automated_versioning/helper/global/versioning_helper.rb
+++ lib/fastlane/plugin/automated_versioning/helper/global/versioning_helper.rb
@@ -1,12 +1,16 @@
 require "fastlane_core/ui/ui"
+require_relative "../files/files_helper.rb"
 
 module Fastlane
-  # UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
+  UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
 
   module Helper
     class VersioningHelper
       def self.check_versioning_config_file
         UI.message("Going to check if versioning file exists")
+        files_helper = Helper::FilesHelper
+        is_conf_file = files_helper.check_file_existence(file_path: "./versioning.conf.json")
+        UI.message("File exists ? #{is_conf_file}")
       end
     end
   end
